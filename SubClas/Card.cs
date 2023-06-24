53184075b7cb427926f4e117b5795a2f476a50fb
Kamil <mentos.96@o2.pl>
2023-06-24 03:19:49 +0200

    DO dokonczenia ustawianeie zachowania po kliknieciu na listboxitem

---
+++ SubClas/Card.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Game_Card
+{
+    class Card
+    {
+        public string Value { get; private set; }
+        public string Suit { get; private set; }
+        public string Name { get { return $"{Value} {Suit}"; }  }
+
+        public Card(EValue value, ESuit suit)
+        {
+            this.Value = value.ToString();
+            this.Suit = suit.ToString();
+        }
+
+        public override string ToString()
+        {
+            return $"{Value} {Suit}";
+        }
+    }
+}
